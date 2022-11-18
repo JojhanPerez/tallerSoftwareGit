2b2e5709aeae981d51b6e6cad7293fa4cfe8a343
JojhanPerez <jojhan.1702011305@ucaldas.edu.co>
2022-11-18 15:36:09 -0500

    Jojhan

---
+++ ProcesoInscripciones.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ProcesoInscripciones {
+
+    private HashMap<String, Estudiante> estudiantes;
+    
+    public List<String> procesarArchivo(String ruta) {
+        return null;
+    }
+
+    private void procesarLinea(String linea) {
+
+    }
+
+    private Estudiante buscarEstudiante(String cedula) {
+        return null;
+    }
+}
