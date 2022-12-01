b4eed4855101d9c5698402400ac315699c07acfe
JojhanPerez <jojhan.1702011305@ucaldas.edu.co>
2022-11-30 23:33:28 -0500

    Implemetación de la clase estudiante

--- inscripciones/Estudiante.java
+++ inscripciones/Estudiante.java
@@ -1,23 +1,28 @@
 import java.util.Set;
 
-public class Estudiante{
+public class Estudiante {
     private String cedula;
     private String nombre;
     private Set<String> codigosMaterias;
 
-    public Estudiante(String cedula, String nombre){
+    public Estudiante(String cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
     }
 
-    public boolean adicionarMateria(String codigoMateria){
-        return true;
+    public boolean adicionarMateria(String codigoMateria) {
+
+        if (codigoMateria != null) {
+            this.codigosMaterias.add(codigoMateria);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     @Override
     public String toString() {
-        // TODO Auto-generat
-        return super.toString();
+        return super.toString() + "Cedula: " + this.cedula + " nombre: " + this.nombre ;
     }
 }
-
