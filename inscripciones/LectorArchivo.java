d97d586e5baf49054a15ac462c6fb539016682bc
JojhanPerez <jojhan.1702011305@ucaldas.edu.co>
2022-11-30 23:56:12 -0500

    Implementación de lector archivo

--- inscripciones/LectorArchivo.java
+++ inscripciones/LectorArchivo.java
@@ -1,12 +1,30 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LectorArchivo {
+    
     public LectorArchivo() {
 
     }
 
-    public List<String> leer(String ruta) {
-        return null;
+    public List<String> leer(String ruta) throws IOException {
+        
+        List<String> listaDatos = new ArrayList<String>();
+        File lectorArchivo = new File(ruta);
+        BufferedReader lector = new BufferedReader(new FileReader(lectorArchivo));
+
+        String linea;
+
+        while ((linea = lector.readLine()) != null) {
+            listaDatos.add(linea);
+        }
+
+        return listaDatos;
     }
 
 }
